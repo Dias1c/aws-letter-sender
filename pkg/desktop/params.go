d9752ed78fa982399eec2d601d5f1f3590cdffb2
Dias1c <dias.kappasov@gmail.com>
2023-01-15 05:11:55 +0600

    add: desktop/params

---
+++ pkg/desktop/params.go
@@ -0,0 +1,69 @@
+package desktop
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	"github.com/Dias1c/aws-letter-sender/internal/letter/sender"
+)
+
+type Params struct {
+	DataFile string
+
+	EmailSender string
+	EmailRegion string
+
+	LetterSubject string
+
+	TemplateFile string
+}
+
+func GetParams() (*Params, error) {
+	awsParams := sender.GetAWSParams()
+	params := &Params{
+		EmailSender: awsParams.SenderEmail,
+		EmailRegion: awsParams.SenderRegion,
+	}
+
+	var (
+		emailSender = flag.String("email-sender", "", `[optional]
+aws sender email
+`)  // --email-sender="
+		subject = flag.String("subject", "", `[optional] 
+letter subject value, if empty, takes subject 
+from data file
+`)  // --subject="name" - subject
+		dataFile = flag.String("data-file", "", fmt.Sprintf(`[required]
+file which stores emails to send and data for 
+using template. It must contain values for key
+'EMAIL'. If file contain 
+%q keys,
+it will use them as main params to send letter.
+`, lineVars))  // --data-file="filepath" - path of data ext file
+		tmplFile = flag.String("tmpl-file", "", `[optional]
+template file. Files must end with '.txt', 
+'.text', '.html'
+`)  // --tmpl-file="path" -
+	)
+	flag.Parse()
+
+	if *emailSender != "" {
+		params.EmailSender = *emailSender
+	}
+	if *subject != "" {
+		params.LetterSubject = *subject
+	}
+
+	if *dataFile == "" {
+		return nil, errors.New("no data file")
+	} else {
+		params.DataFile = *dataFile
+	}
+
+	if *tmplFile != "" {
+		params.TemplateFile = *tmplFile
+	}
+
+	return params, nil
+}
