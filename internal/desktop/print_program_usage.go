e79d4b1e71358696e29612eddb1e1cd10e4939d8
Dias1c <dias.kappasov@gmail.com>
2025-02-02 15:14:23 +0500

    feat: add description to usage

--- internal/desktop/print_program_usage.go
+++ internal/desktop/print_program_usage.go
@@ -9,7 +9,8 @@ import (
 
 func printProgramUsage() {
 	fmt.Println("Program Name:", configs.GetExpectedProgramFullName())
-	fmt.Println("Usage:", configs.SourceCodeLink)
+	fmt.Println("Description:  Bulk emails using AWS SES with golang HTML/text templates support")
+	fmt.Println("Usage:       ", configs.SourceCodeLink)
 	fmt.Println()
 	fmt.Println("Flags:")
 	flag.PrintDefaults()
