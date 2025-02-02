60b02bb82b61cfaf711f327d864ad4ef2bf6cc1f
Dias1c <dias.kappasov@gmail.com>
2025-02-02 15:32:47 +0500

    fix: ProgramName

--- internal/configs/configs.go
+++ internal/configs/configs.go
@@ -9,7 +9,7 @@ var Vesrion = "x.y.z_dev"
 var VersionSourceCodeLink = fmt.Sprintf("https://github.com/Dias1c/aws-ses-bulk-emails/releases/tag/v%s", Vesrion)
 
 const SourceCodeLink = "https://github.com/Dias1c/aws-ses-bulk-emails"
-const ProgramName = "aws-ses-bulk-emails"
+const ProgramName = "aws_ses_bulk_emails"
 
 func GetExpectedProgramFullName() string {
 	return fmt.Sprintf("%v-v%v-%v-%v", ProgramName, Vesrion, runtime.GOOS, runtime.GOARCH)
